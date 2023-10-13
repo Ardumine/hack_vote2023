4fe2b3db8e11400048d372f983ce126572c1a836
david.codeco.gmail.com <david.codeco@gmail.com>
2023-10-13 23:14:59 +0100

    Adicionar arquivos de projeto.

---
+++ hack_vote2023/Program.cs
@@ -0,0 +1,14 @@
+namespace hack_vote2023
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.Run(new main());
+        }
+    }
+}
---
+++ hack_vote2023/main.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hack_vote2023
+{
+    public partial class main : Form
+    {
+        void exec_cmd(string cmd)
+        {
+            SendKeys.Send(cmd.Replace(Environment.NewLine, ""));
+            Console.Beep(200, 200);
+            SendKeys.Send("{ENTER}");
+            Console.Beep(400, 300);
+
+        }
+        public main()
+        {
+            InitializeComponent();
+            Hide();
+        }
+        void wait(int tempo, int epoch)
+        {
+            for (int i = 0; i < 15; i++)
+            {
+                SendKeys.Send("{BS}");
+            }
+            for (int i = 0; i < tempo; i+=500)
+            {
+                string txt = epoch.ToString() + "|" + ((float)i / 1000).ToString();
+                SendKeys.Send(txt);
+                
+                Thread.Sleep(500);
+
+                for (int l = 0; l < txt.Length + 1; l++)
+                {
+                    SendKeys.Send("{BS}");
+                }
+            }
+        }
+        string cords_pausa = "-85 -2 -89";
+        int tempo_pausa_curto = 15;//segundos
+        void pausa_curta(int epoch)
+        {
+            var cmd = ".tp " + cords_pausa;
+            Console.WriteLine($"[Pausa curta] ->{cmd}");
+            exec_cmd(cmd);
+            Console.Beep(100, 50);
+            
+            wait(tempo_pausa_curto * 1000, epoch);
+
+            Console.Beep(500, 100);
+        }
+        void Fazer_parkour(string nom_fich, int i)
+        {
+            List<string> Dados = File.ReadAllLines(cam + nom_fich).ToList();
+            foreach (string dado in Dados)
+            {
+                if (!dado.StartsWith("!"))
+                {
+                    var cmd = ".tp " + dado;
+                    Console.WriteLine($"[{i}, {nom_fich}] ->{cmd}");
+                    exec_cmd(cmd);
+                    Thread.Sleep(200);
+                }
+                Console.WriteLine($"[{i}, {nom_fich}] Conc!");
+
+            }
+        }
+
+            string cam = "C:\\Users\\david\\OneDrive\\Documentos\\projectos_csharp\\hack_vote2023\\data\\";
+
+        private void main_Load(object sender, EventArgs e)
+        {
+            Thread.Sleep(4000);
+            Console.Beep();
+            SendKeys.Send("t");
+            Console.Beep(100, 200);
+            var nomes = new string[] {"penguin.txt"};
+            for (int i = 0; i < 30; i++)
+            {
+                foreach(string nome in nomes)
+                {
+                    Fazer_parkour(nome,i);
+                    Console.Beep(500, 300);
+                    Console.Beep(800, 300);
+                    Console.Beep(500, 300);
+                    pausa_curta(i);
+
+                }
+                Console.WriteLine($"[{i}] Conc!");
+                Console.WriteLine();
+                Console.WriteLine();
+
+            }
+
+
+        }
+    }
+}
